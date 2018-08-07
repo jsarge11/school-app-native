7abfa27405e650ddceec021589f7b307471464cb
Jay Sargent <sargentjay11@gmail.com>
2018-08-07 10:28:30 -0600

    showing all problem sets

--- student-app/components/Assessments/Multiplication.js
+++ student-app/components/Assessments/Multiplication.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react'
 import { View, Text, Alert, Button, StyleSheet } from 'react-native'
+import axios from 'axios'
 
 
 export default class Multiplication extends Component {
+    state = {
+        problem_sets: [],
+    }
+componentDidMount() {
+    axios.get('http://10.0.0.74:4000/math/problems?id=1').then(res => {
+        this.setState({ problem_sets: res.data })
+    })
+}
 render() {
+    let problem_sets = this.state.problem_sets.map(problem_set => {
+        return (
+            <Button
+                key={problem_set.ps_id}
+                title={problem_set.name}
+                onPress={() => console.log(problem_set.ps_id)}
+            />
+        )
+    })
         return (
            <View style={styles.container}>
             <Text>Multiplication</Text>
+            {problem_sets}
            </View>
         )
     }
